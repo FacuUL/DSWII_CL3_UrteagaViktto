83da424a228792e21e7cbf596e15d0bcce097c74
FacuUL <facundodioses05@gmail.com>
2023-11-25 21:14:29 -0500

    first commit

---
+++ src/main/java/com/cibertec/edu/pe/DSWII_CL3_UrteagaViktto/DswiiCl3UrteagaVikttoApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec.edu.pe.DSWII_CL3_UrteagaViktto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiCl3UrteagaVikttoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiCl3UrteagaVikttoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cibertec/edu/pe/DSWII_CL3_UrteagaViktto/DswiiCl3UrteagaVikttoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec.edu.pe.DSWII_CL3_UrteagaViktto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiiCl3UrteagaVikttoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
